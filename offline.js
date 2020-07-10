fcac35cc241b0902b54a1f43a93584fbb69d3608
zanjaprogrammer <Bismillah101@101>
2020-07-10 11:35:26 +0700

    waw

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1594355613,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite3-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"media/netherplace_looping.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
